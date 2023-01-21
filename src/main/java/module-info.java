76214162a359e757a56ad556b446620abef5462a
varglaszlo <va.laszlo91@gmail.com>
2023-01-21 14:49:21 +0100

    try

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,9 @@
 module com.example.firstjavafx {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                requires validatorfx;
-                requires org.kordamp.bootstrapfx.core;
-            
+
+    requires org.kordamp.bootstrapfx.core;
+
     opens com.example.firstjavafx to javafx.fxml;
     exports com.example.firstjavafx;
 }
